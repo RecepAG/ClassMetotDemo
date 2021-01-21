092e4fc4b4b7cc59bec4817f6f8eb002f8921a9d
RecepAG <gokmenrecepali@gmail.com>
2021-01-21 20:14:35 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Ekle(Customer musteri)
+        {
+            Console.WriteLine("Müşteri Numarası : " +musteri.Id);
+            Console.WriteLine("Müşteri Adı : "+musteri.CustomerName);
+            Console.WriteLine("Müşteri Soyadı : "+ musteri.CustomerSurname);
+            Console.WriteLine("Doğum Tarihi : "+musteri.BirthDate);
+            Console.WriteLine("Doğum Yeri : "+musteri.BirthPlace);
+        }
+        public void Sil (Customer musteri)
+        {
+            Console.WriteLine("Müşteri Başarıyla Silindi :" + musteri.CustomerName+" "+musteri.CustomerSurname);
+        }
+
+        public void Listele(Customer[] musteri)
+        {
+            Console.WriteLine("--------Müşteri Listesi-----------");
+            foreach (Customer x in musteri)
+            {
+                Console.WriteLine("Müşteri Numarası : " + x.Id);
+                Console.WriteLine("Ad ve Soyad : " + x.CustomerName + " " + x.CustomerSurname);
+
+            }
+        }
+
+        
+        
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer musteri1 = new Customer();
+            musteri1.Id = 11111111;
+            musteri1.CustomerName = "Mehmet";
+            musteri1.CustomerSurname = "Çavdar";
+            musteri1.BirthDate = "10.02.1985";
+            musteri1.BirthPlace = "İzmir";
+
+            Customer musteri2 = new Customer();
+            musteri2.Id = 22222222;
+            musteri2.CustomerName = "Ahmet";
+            musteri2.CustomerSurname = "Uslu";
+            musteri2.BirthDate = "01.12.1985";
+            musteri2.BirthPlace = "İstanbul";
+
+            Customer[] musteriler = new Customer[] { musteri1,musteri2 };
+
+            CustomerManager musteriManager = new CustomerManager();
+            musteriManager.Listele(musteriler);
+
+
+            Console.WriteLine("--------Müşteri Detayları----------");
+
+
+            CustomerManager musteriManager1 = new CustomerManager();
+            musteriManager1.Ekle(musteri1);
+            Console.WriteLine("-----------------------------------");
+            musteriManager1.Ekle(musteri2);
+            Console.WriteLine("-----------------------------------");
+            musteriManager1.Sil(musteri1);
+            Console.WriteLine("-----------------------------------");
+            musteriManager1.Sil(musteri2);
+
+
+
+
+        }
+
+    } 
+}
